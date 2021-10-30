fcc2ca9370a252f9e4865d8441ac4a5663e34b4a
Santiago Hewitt Ramirez <hewitt@une.net.co>
2021-10-29 21:50:24 -0500

    se actualiza el routes

---
+++ backend/routes/index.js
@@ -0,0 +1,7 @@
+const product_routes = require('./product.routes');
+const sales_routes = require('./sales.routes');
+const users_routes = require('./user.routes');
+
+module.exports = Object.freeze({
+    product_routes,
+});
---
+++ backend/routes/users.routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const {users_controller} = require('../controllers');
+const {users_middleware} = require('../middlewares');
+
+
+router.post('/add',users_middleware.verifyTypes, users_middleware.verifyid, users_controller.addUsers);
+router.get('/list',users_controller.getAllUsers); 
+router.put('/update',users_middleware.verifyTypes, users_controller.updateUsers); 
+router.delete('/delete/:id',users_controller.deleteUsers);
+
+module.exports=router;
