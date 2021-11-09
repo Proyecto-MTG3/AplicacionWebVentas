d6432267bc4969b2d37fcb6cd1ca2821cb24e703
Santiago Hewitt Ramirez <hewitt@une.net.co>
2021-11-08 19:09:16 -0500

    se cambia datos en URI de Mongo

--- backend/App.js
+++ backend/App.js
@@ -14,13 +14,13 @@ app.use(cors());
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-
+const port = process.env.PORT ||3001;
 
 //Configurar la conexion con mongo atlas
 const mongoose = require('mongoose');
-const uri = `mongodb+srv://${process.env.User}:${process.env.PASSWORD}@cluster0.l4uir.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`
+const URI = 'mongodb+srv://AplicacionWebVentas:ROjYw8UuU3nqOGiw@cluster0.l4uir.mongodb.net/myFirstmitg3?retryWrites=true&w=majority'
 const option ={useNewUrlParser: true, useUnifiedTopology: true};
-mongoose.connect(uri, option)
+mongoose.connect(URI, option)
 .then(()=> console.log("Base de datos Conectada correctamente"))
 .catch((e)=> console.log("Error en la conexion db:",e));
 
@@ -28,7 +28,7 @@ mongoose.connect(uri, option)
 const{product_routes} = require ('./routers');
 const{sales_routes} = require ('./routers');
 const{users_routes} = require ('./routers');
-const auth = require('registry-auth-token');
+//const auth = require('registry-auth-token');
 
 //uso de las rutas                                              
 app.use('/api/v1/product',product_routes);
@@ -39,4 +39,6 @@ app.use('/api/v1/users/',users_routes);
 
 
 //Nuestro servidor debe estar escuchando
-app.listen(process.env.PORT,()=> console.log('Servidor a su servico',process.env.PORT));
+
+app.listen(port ,()=> console.log('Servidor a su servico ${port}'));
+//app.listen(process.env.PORT,()=> console.log('Servidor a su servico',process.env.PORT));
